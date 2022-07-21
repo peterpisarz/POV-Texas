00e7a778349dbe04ff1af48797739a5c8072fce1
Peter Pisarz <peter.pisarz@gmail.com>
2022-07-21 18:55:23 -0500

    Updated Navbar Link to IG

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ const Navbar = ({ web3Handler, account, explorerURL }) => {
         <nav className="navbar fixed-top mx-3">
             <a
                 className="navbar-brand col-sm-3 col-md-2 mr-0 mx-4"
-                href="http://www.dappuniversity.com/bootcamp"
+                href="https://www.instagram.com/hollystcrypto/"
                 target="_blank"
                 rel="noopener noreferrer"
             >
